c7c1336e5a003f9aa9578c93f35006ff3f321d3a
Alexsandro <alexsandro.oliveira117@gmail.com>
2022-06-04 11:23:53 -0400

    GetLatest: Function update --> fixing some issues

--- src/mangaforest.go
+++ src/mangaforest.go
@@ -24,10 +24,6 @@ func GetmangaforestLatestUpdatesPage() []model.Manga {
 			log.Fatal(err.Error())
 		}
 		defer res.Body.Close()
-		//TODO:find a way to not break the application
-		if res.StatusCode != http.StatusOK {
-			log.Fatalf("Statuscode error: %d %s", res.StatusCode, res.Status)
-		}
 
 		doc, err := goquery.NewDocumentFromReader(res.Body)
 		//TODO:find a way to not break the application
@@ -38,15 +34,9 @@ func GetmangaforestLatestUpdatesPage() []model.Manga {
 		doc.Find("div.book-item").Find("div.book-detailed-item").Each(
 			func(i int, s *goquery.Selection) {
 				node := s.Find("div.thumb")
-				title, titleExists := node.Find("a").Attr("title")
-				path, pathExist := node.Find("a").Attr("href")
+				title, _ := node.Find("a").Attr("title")
+				path, _ := node.Find("a").Attr("href")
 
-				//TODO:Change to only omit the atributes that doesn't exist~.
-				if !pathExist || !titleExists {
-					listaMangas = append(listaMangas,
-						model.Manga{Title: title, Path: "", SiteURL: mangaforestBaseURL, ImgURL: ""})
-					return
-				}
 				listaMangas = append(listaMangas,
 					model.Manga{Title: title, Path: path, SiteURL: mangaforestBaseURL, ImgURL: ""})
 			},
--- src/readm.go
+++ src/readm.go
@@ -24,31 +24,21 @@ func GetreadmLatestUpdatesPage() []model.Manga {
 			log.Fatal(err.Error())
 		}
 		defer res.Body.Close()
-		//TODO:find a way to not break the application
-		if res.StatusCode != http.StatusOK {
-			log.Fatalf("Statuscode error: %d %s", res.StatusCode, res.Status)
-		}
 
 		doc, err := goquery.NewDocumentFromReader(res.Body)
 		//TODO:find a way to not break the application
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		//TODO: Try to change it for []struct instead of an [][]string
+
 		doc.Find("ul.clearfix").Find("li.segment-poster-sm").Each(
 			func(i int, s *goquery.Selection) {
 				node := s.Find("h2")
 				title := node.Find("a").Text()
-				path, pathExist := node.Find("a").Attr("href")
+				path, _ := node.Find("a").Attr("href")
 				node = s.Find("img")
-				imgPath, imgPathExist := node.Attr("data-src")
+				imgPath, _ := node.Attr("data-src")
 
-				//TODO:Change to only omit the atributes that doesn't exist~.
-				if !pathExist || !imgPathExist {
-					listaMangas = append(listaMangas,
-						model.Manga{Title: title, Path: "", SiteURL: readmBaseURL, ImgURL: ""})
-					return
-				}
 				listaMangas = append(listaMangas,
 					model.Manga{Title: title, Path: path, SiteURL: readmBaseURL, ImgURL: imgPath})
 			},
