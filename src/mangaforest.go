839d496b56dddf29b46c165a1884ac1bb53cf27c
Alexsandro <alexsandro.oliveira117@gmail.com>
2022-06-04 03:41:06 -0400

    GetLatestFunctions Update - get the last 5 pages of updates.

--- src/mangaforest.go
+++ src/mangaforest.go
@@ -1,6 +1,7 @@
 package scraper
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"shampoo-scraper/src/model"
@@ -10,44 +11,46 @@ import (
 
 var mangaforestBaseURL = "https://mangaforest.com"
 
-//TODO:get also de upates from the second page
 func GetmangaforestLatestUpdatesPage() []model.Manga {
-	latestUpdatePageURL := mangaforestBaseURL + "/latest"
 	var listaMangas []model.Manga
-	res, err := http.Get(latestUpdatePageURL)
 
-	//TODO:find a way to not break the application
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer res.Body.Close()
-	//TODO:find a way to not break the application
-	if res.StatusCode != http.StatusOK {
-		log.Fatalf("Statuscode error: %d %s", res.StatusCode, res.Status)
-	}
+	for i := 1; i < 6; i++ {
+		latestUpdatePageURL := mangaforestBaseURL + fmt.Sprintf("/latest?page=%d", i)
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	//TODO:find a way to not break the application
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+		res, err := http.Get(latestUpdatePageURL)
 
-	doc.Find("div.book-item").Find("div.book-detailed-item").Each(
-		func(i int, s *goquery.Selection) {
-			node := s.Find("div.thumb")
-			title, titleExists := node.Find("a").Attr("title")
-			path, pathExist := node.Find("a").Attr("href")
+		//TODO:find a way to not break the application
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		defer res.Body.Close()
+		//TODO:find a way to not break the application
+		if res.StatusCode != http.StatusOK {
+			log.Fatalf("Statuscode error: %d %s", res.StatusCode, res.Status)
+		}
 
-			//TODO:Change to only omit the atributes that doesn't exist~.
-			if !pathExist || !titleExists {
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		//TODO:find a way to not break the application
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		doc.Find("div.book-item").Find("div.book-detailed-item").Each(
+			func(i int, s *goquery.Selection) {
+				node := s.Find("div.thumb")
+				title, titleExists := node.Find("a").Attr("title")
+				path, pathExist := node.Find("a").Attr("href")
+
+				//TODO:Change to only omit the atributes that doesn't exist~.
+				if !pathExist || !titleExists {
+					listaMangas = append(listaMangas,
+						model.Manga{Title: title, Path: "", SiteURL: mangaforestBaseURL, ImgURL: ""})
+					return
+				}
 				listaMangas = append(listaMangas,
-					model.Manga{Title: title, Path: "", SiteURL: mangaforestBaseURL, ImgURL: ""})
-				return
-			}
-			listaMangas = append(listaMangas,
-				model.Manga{Title: title, Path: path, SiteURL: mangaforestBaseURL, ImgURL: ""})
-		},
-	)
-	//TODO: Try to change it for []struct instead of an [][]string
+					model.Manga{Title: title, Path: path, SiteURL: mangaforestBaseURL, ImgURL: ""})
+			},
+		)
+	}
 	return listaMangas
 }
--- src/readm.go
+++ src/readm.go
@@ -1,6 +1,7 @@
 package scraper
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"shampoo-scraper/src/model"
@@ -10,46 +11,48 @@ import (
 
 var readmBaseURL = "https://www.readm.org"
 
-//TODO:get also de upates from the second page
 func GetreadmLatestUpdatesPage() []model.Manga {
-	latestUpdatePageURL := readmBaseURL + "/latest-releases"
 	var listaMangas []model.Manga
-	res, err := http.Get(latestUpdatePageURL)
 
-	//TODO:find a way to not break the application
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer res.Body.Close()
-	//TODO:find a way to not break the application
-	if res.StatusCode != http.StatusOK {
-		log.Fatalf("Statuscode error: %d %s", res.StatusCode, res.Status)
-	}
+	for i := 1; i < 6; i++ {
+		latestUpdatePageURL := readmBaseURL + fmt.Sprintf("/latest-releases/%d", i)
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	//TODO:find a way to not break the application
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	//TODO: Try to change it for []struct instead of an [][]string
-	doc.Find("ul.clearfix").Find("li.segment-poster-sm").Each(
-		func(i int, s *goquery.Selection) {
-			node := s.Find("h2")
-			title := node.Find("a").Text()
-			path, pathExist := node.Find("a").Attr("href")
-			node = s.Find("img")
-			imgPath, imgPathExist := node.Attr("data-src")
+		res, err := http.Get(latestUpdatePageURL)
 
-			//TODO:Change to only omit the atributes that doesn't exist~.
-			if !pathExist || !imgPathExist {
+		//TODO:find a way to not break the application
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		defer res.Body.Close()
+		//TODO:find a way to not break the application
+		if res.StatusCode != http.StatusOK {
+			log.Fatalf("Statuscode error: %d %s", res.StatusCode, res.Status)
+		}
+
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		//TODO:find a way to not break the application
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		//TODO: Try to change it for []struct instead of an [][]string
+		doc.Find("ul.clearfix").Find("li.segment-poster-sm").Each(
+			func(i int, s *goquery.Selection) {
+				node := s.Find("h2")
+				title := node.Find("a").Text()
+				path, pathExist := node.Find("a").Attr("href")
+				node = s.Find("img")
+				imgPath, imgPathExist := node.Attr("data-src")
+
+				//TODO:Change to only omit the atributes that doesn't exist~.
+				if !pathExist || !imgPathExist {
+					listaMangas = append(listaMangas,
+						model.Manga{Title: title, Path: "", SiteURL: readmBaseURL, ImgURL: ""})
+					return
+				}
 				listaMangas = append(listaMangas,
-					model.Manga{Title: title, Path: "", SiteURL: readmBaseURL, ImgURL: ""})
-				return
-			}
-			listaMangas = append(listaMangas,
-				model.Manga{Title: title, Path: path, SiteURL: readmBaseURL, ImgURL: imgPath})
-		},
-	)
-
+					model.Manga{Title: title, Path: path, SiteURL: readmBaseURL, ImgURL: imgPath})
+			},
+		)
+	}
 	return listaMangas
 }
