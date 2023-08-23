bd9c7aa97f8e74b67b31e8bfcd99486df7ce73d5
Alexsandro Oliveira Alves <alexsandro.oliveira117@gmail.com>
2023-08-23 01:32:19 -0300

    Adding get latest ch number

--- src/scrapers/asuraScraper.go
+++ src/scrapers/asuraScraper.go
@@ -27,21 +27,26 @@ func checkAsuraConfigFileIsLoaded() {
 	}
 }
 
-func AsuraFindSerieUrlByName(name string) string {
-	checkAsuraConfigFileIsLoaded()
-	reqName := strings.ReplaceAll(name, " ", asuraConfig["separator"])
-	rootNode := hw.GetUrl(fmt.Sprintf(asuraConfig["baseUrl"] + asuraConfig["searchUrl"] + reqName))
-	node := hw.SearchFirstNodeOccurrence(rootNode, "title", name, "a")
+func returnValFromKey(node *html.Node, key string) string {
 	var actualSeriesUrl string
 
 	for _, a := range node.Attr {
 		if a.Key == "href" {
 			{
 				actualSeriesUrl = a.Val
+				break
 			}
 		}
 	}
 	return actualSeriesUrl
+}
+
+func AsuraFindSerieUrlByName(name string) string {
+	checkAsuraConfigFileIsLoaded()
+	reqName := strings.ReplaceAll(name, " ", asuraConfig["separator"])
+	rootNode := hw.GetUrl(fmt.Sprintf(asuraConfig["baseUrl"] + asuraConfig["searchUrl"] + reqName))
+	node := hw.SearchFirstNodeOccurrence(rootNode, "title", name, "a")
+	return returnValFromKey(node, "href")
 }
 
 func AsuraGetLestEpUrl(url string) string {
@@ -55,5 +60,21 @@ func AsuraGetLestEpUrl(url string) string {
 		utils.ErrLogOutput(err)
 	}
 	n := hw.SearchFirstNodeOccurrence(rootNode, "class", "eph-num", "div")
+	//create method to find a node inside anothern node
 	return n.FirstChild.NextSibling.Attr[0].Val
+}
+
+func AsuraGetLestEpNumber(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		utils.ErrLogOutput(err)
+	}
+	defer resp.Body.Close()
+	rootNode, err := html.Parse(resp.Body)
+	if err != nil {
+		utils.ErrLogOutput(err)
+	}
+	n := hw.SearchFirstNodeOccurrence(rootNode, "class", "eph-num", "div")
+	//create method to find a node inside anothern node
+	return n.FirstChild.NextSibling.FirstChild.NextSibling.FirstChild.Data
 }
