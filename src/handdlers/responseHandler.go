668ba8331706f11658e28c16796d069a3fc2169b
Alexsandro Oliveira Alves <alexsandro.oliveira117@gmail.com>
2023-08-28 23:13:10 -0300

    Adding commernts to the structs

--- src/handdlers/responseHandler.go
+++ src/handdlers/responseHandler.go
@@ -1,11 +1,17 @@
 package handdlers
 
+/*
+Respose struct
+*/
 type response struct {
 	method     string
 	errMessage string
 	resp       string
 }
 
+/*
+Respose struct to handle the system reponses
+*/
 type responsesStruct struct {
 	status    string
 	responses []response
